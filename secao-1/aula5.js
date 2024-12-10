193bafef417efd15817f5afcbe9b9a6cc1108602
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-10 19:08:06 -0300

    Seção 1: Aula 5 - Variáveis com let

---
+++ secao-1/aula5.js
@@ -0,0 +1,15 @@
+let nome = 'Otávio'; // String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000,', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome,'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo');
+
+let nome2; // Declarou a variável
+
+nome2 = 'qualquer valor'; // Inicializando a variável
+console.log(nome2);
+
+nome2 = 'um outro valor';
+console.log(nome2);
