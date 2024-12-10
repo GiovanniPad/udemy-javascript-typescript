a0ea04b6c8cfcaccb61c8bbc1f119e11b3c17775
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-10 17:34:20 -0300

    Seção 1: Aula 3 - Comentários de código

---
+++ secao-1/aula3.js
@@ -0,0 +1,17 @@
+// Comentário em linha
+console.log('Olá, Mundo!'); // Comentário na frente do código
+
+console.log('Linha 1');
+//console.log('Linha 2');
+// Linha que vai ser ignorada.
+console.log('Linha 3');
+
+/**
+ * Um comentário em bloco
+ * 
+ * Linha 1
+ * Linha 2
+ * Linha 3
+ * 
+ * Final do comentário em bloco
+*/
