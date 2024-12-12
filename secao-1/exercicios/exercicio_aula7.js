8b1735413c25f9ba5b3fe09a5ccc9ad89c898178
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-12 18:38:09 -0300

    Seção 1: Aula 7 - Exercícios - const, let e Template String

---
+++ secao-1/exercicios/exercicio_aula7.js
@@ -0,0 +1,15 @@
+const nome = 'Giovanni Padilha';
+const idade = 23;
+const peso = 89;
+const alturaEmM = 1.73;
+
+let indiceMassaCorporal;
+let anoNascimento;
+
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2024 - idade;
+
+console.log(`${nome} tem ${idade} anos, pesa ${peso}kg tem ${alturaEmM}m de altura e seu IMC é de ${indiceMassaCorporal}.`);
+
+// Template String
+console.log(`${nome} nasceu em ${anoNascimento}.`);
