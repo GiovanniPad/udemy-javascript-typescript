67d5224d0f7f24d61ad772d69e250686b0bcbfb1
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-11 21:24:03 -0300

    Seção 1: Aula 6 - Constantes com const

---
+++ secao-1/aula6.js
@@ -0,0 +1,20 @@
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+
+let texto = '5';
+
+console.log(typeof primeiroNumero);
+console.log(typeof texto);
+console.log(typeof(primeiroNumero + segundoNumero));
