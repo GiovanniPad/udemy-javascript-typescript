7838c91eedf2236a0d4427cf8720c705bfe6b395
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-13 22:13:35 -0300

    Seção 1: Aula 9 - Tipos de dados primitivos

---
+++ secao-1/aula9.js
@@ -0,0 +1,19 @@
+// string
+const nome1 = 'Giovanni';
+const nome2 = "Padilha";
+// template string
+const nome3 = `Giovanni Padilha`;
+
+// number
+const num1 = 10;
+const num2 = 10.52;
+
+// undefined e null
+let nomeAluno;
+let sobrenomeAluno = null;
+
+// boolean
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(typeof nome1, typeof num1, typeof nomeAluno, typeof sobrenomeAluno, typeof boolean1);
