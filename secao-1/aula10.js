3624ab5cafe88cb0e3a7224c70b13df1cb04e1c0
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-14 22:28:59 -0300

    Seção 1: Aula 10 - Operadores Aritméticos, de Atribuição e Incremento

---
+++ secao-1/aula10.js
@@ -0,0 +1,58 @@
+// Aritméticos
+
+const num1 = 2;
+const num2 = 10;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** 10);
+console.log(num1 % num2);
+
+console.log('---------------------------');
+
+// Incremento
+
+let contador = 1;
+console.log(contador++);
+console.log(++contador);
+
+console.log(contador--);
+console.log(--contador);
+
+console.log('---------------------------');
+
+// Atribuição
+
+let contador2 = 2;
+
+contador2 += 5;
+console.log(contador2);
+
+contador2 -= 3;
+console.log(contador2);
+
+contador2 *= 3;
+console.log(contador2);
+
+contador2 /= 2;
+console.log(contador2);
+
+contador2 **= 2;
+console.log(contador2);
+
+const num3 = 2;
+const num4 = 'Giovanni';
+const num5 = '2'
+
+console.log(num3 * num4);
+console.log(num3 * num5);
+
+const num7 = parseInt('5.2');
+const num8 = parseFloat('5.2');
+const num9 = Number('5.32');
+
+console.log(num7);
+console.log(num8);
+console.log(num9);
