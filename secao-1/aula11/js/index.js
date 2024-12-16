62e12d7d3a6738add3242b7ff28b9e3e73c1a89f
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-16 18:59:17 -0300

    Seção 1: Aula 11 - alert, confirm e prompt (Navegador)

---
+++ secao-1/aula11/js/index.js
@@ -0,0 +1,16 @@
+alert('Mensagem usando alert a partir do arquivo JS');
+
+const resposta = confirm('Confirmar uma ação');
+console.log(resposta);
+
+const mensagem = prompt('Escreve uma mensagem');
+console.log(mensagem);
+
+// Exercício da aula
+
+let num1 = Number(prompt('Digite um número'));
+let num2 = Number(prompt('Digite outro número'));
+
+let resultado = num1 + num2;
+
+alert('O resultado foi de ' + resultado); 
