45801dad5ac1aae998b8e6968379a2aea367e092
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-31 15:01:20 -0300

    Seção 1: Aula 22 - Objetos - Introdução

---
+++ secao-1/aula22.js
@@ -0,0 +1,30 @@
+function criaPessoa(nome, sobrenome, idade) {
+    return {
+        nome,
+        sobrenome,
+        idade,
+
+        fala() {
+            console.log(`Eu me chamo ${this.nome} ${this.sobrenome}!`);
+        },
+
+        incrementaIdade() {
+            this.idade++;
+        }
+    };
+}
+
+const pessoa1 = criaPessoa('Giovanni', 'Padilha', 23);
+const pessoa2 = criaPessoa('Beatriz', 'Ricci', 21);
+const pessoa3 = criaPessoa('Zeca', 'Joe', 10);
+
+console.log(pessoa1.nome);
+console.log(pessoa2.sobrenome);
+console.log(pessoa3.idade);
+
+pessoa2.fala();
+
+pessoa3.incrementaIdade();
+console.log(pessoa3.idade);
+pessoa3.incrementaIdade();
+console.log(pessoa3.idade);
