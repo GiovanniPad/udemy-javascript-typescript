8409ff3089c5b0a103c097af32c5ced9e9582803
Giovanni Padilha <giovanni.padilha@outlook.com>
2024-12-31 15:28:53 -0300

    Seção 1: Aula 23 - Valores Primitivos e Valores por Referência

---
+++ secao-1/aula23.js
@@ -0,0 +1,35 @@
+let a = 'A';
+let b = a; // Cópia
+console.log(a, b);
+
+a = 'Outra coisa';
+console.log(a, b);
+
+let c = [1, 2, 3];
+let d = c; // Referência
+let e = [...d];
+
+console.log(c, d, e);
+
+c.push(4);
+console.log(c, d, e);
+
+d.pop();
+console.log(c, d, e);
+
+e.push(4);
+console.log(c, d, e);
+
+const x = {
+    nome: 'Giovanni',
+    sobrenome: 'Padilha'
+};
+const y = x;
+
+console.log(x, y);
+
+x.nome = 'Beatriz';
+console.log(x, y);
+
+y.nome = 'Zeca';
+console.log(x, y);
