ff8cd937aa458dcbf8e5668793c84b31f2b25d19
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-01-06 19:15:54 -0300

    Seção 2: Aula 25 - Operadores de Comparação

---
+++ secao-2/aula25/index.js
@@ -0,0 +1,20 @@
+// Aula 25 - Operadores de Comparação
+
+
+const comparacao = 10 > 5;
+console.log(comparacao);
+
+console.log(10 > 10);
+console.log(10 >= 10);
+
+console.log(10 < 9);
+console.log(12 <= 12);
+
+const num1 = 10;
+const num2 = '10';
+
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+console.log(num1 != num2);
+console.log(num1 !== num2);
