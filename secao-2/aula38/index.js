0adbbc6852eba49ef0a595b7afeca58a1996a61c
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-01-14 18:56:03 -0300

    Seção 2: Aula 38 - Atribuição via Desestruturação (Objetos)

---
+++ secao-2/aula38/index.js
@@ -0,0 +1,21 @@
+// Aula 38 - Atribuição via Desestruturação (Objetos)
+
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 30,
+    endereco: {
+        rua: 'Av Brasil',
+        numero: 320
+    }
+};
+
+// const { nome: teste = 'Não existe', sobrenome } = pessoa;
+// console.log(teste, sobrenome);
+
+// const { endereco: { rua: r = 12345, numero }, endereco } = pessoa;
+// console.log(r, numero, endereco);
+
+const { nome, sobrenome, ...resto } = pessoa;
+console.log(nome, sobrenome, resto);
