83ba89c6d0a1bf6fa1d9068d974faab97d02beec
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-01-13 23:09:08 -0300

    Seção 2: Aula 36 - Mais Diferenças entre Var, Let e Const

---
+++ secao-2/aula36/index.js
@@ -0,0 +1,44 @@
+// Aula 36 - Mais Diferenças entre Var, Let e Const
+
+
+// const verdadeira = true;
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz'; // criando
+
+// if (verdadeira) {
+//     let nome = 'Otávio'; // criando
+//     var nome2 = 'Rogério'; // redeclarando
+//     console.log(nome, nome2);
+
+//     if (verdadeira) {
+//         var nome2 = 'Ronaldo'; // redeclarada
+//         let nome = 'Outra coisa';
+//         console.log(nome, nome2);
+//     }
+// }
+
+// console.log(nome, nome2);
+
+// let nome = 'Luiz';
+// let sobrenome = 'Miranda';
+
+// function falaOi () {
+//     console.log(sobrenome);
+
+//     if (verdadeira) {
+//         let nome = 'Luiz';
+//         var sobrenome = 'Miranda';
+//         console.log(nome, sobrenome);
+//     }
+
+//     console.log(sobrenome);
+// }
+
+// falaOi();
+
+// Hoisting
+console.log(sobrenome);
+
+
+var sobrenome = 'Miranda';
